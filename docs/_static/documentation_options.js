5c49eaaee12948ecc2f2aff526a9e51e6d4d98b5
George Trojan <george.trojan@gmail.com>
2021-03-22 22:13:07 +0000

    Updated file whats-new

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.2-rc0+15.ge499aed',
+    VERSION: '0.2.2+4.g86370f2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
